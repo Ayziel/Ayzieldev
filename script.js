7c3ac4b44ac80633753652fcd887b15cc56b3107
Ayziel <90187527+Ayziel@users.noreply.github.com>
2023-01-25 07:53:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function SentEmail() {
+  console.log("start");
+  var tempParams = {
+  
+    from_name: document.getElementById("MySubject").value,
+    to_name: document.getElementById("MyName").value,
+    message: document.getElementById("MyMessage").value,
+    email: document.getElementById("MyEmail").value,
+  
+  };
+  
+  emailjs.send('service_ejxuv7g',"template_basw2me",tempParams)
+  .then(function(res){
+    console.log("success", res.status);
+    console.log("in");
+  })
+
+  }
+
+  function func(e)
+  {
+  event.preventDefault()
+  
+  console.log('not refreshed!');
+  
+  }
