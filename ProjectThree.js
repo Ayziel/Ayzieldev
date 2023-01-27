f15f65bb7a26feef0bc52e4c69e7e9a059178cfa
Ayziel <90187527+Ayziel@users.noreply.github.com>
2023-01-27 17:40:18 +0800

    Add files via upload

---
+++ ProjectThree.js
@@ -0,0 +1,11 @@
+function Delay(duration){
+    return new Promise(resolve => {
+        setTimeout(resolve, duration);
+    })
+}
+
+var loader =document.getElementById("preloader");
+window.addEventListener("load", async function(){
+    await Delay(500)
+  loader.style.display = "none";  
+})
