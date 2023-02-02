33e527a3231467795950d05c624257718b439659
Ayziel <90187527+Ayziel@users.noreply.github.com>
2023-02-02 16:05:48 +0800

    Add files via upload

--- Movie.js
+++ Movie.js
@@ -9,3 +9,13 @@ window.addEventListener("load", async function(){
     await Delay(0)
   loader.style.display = "none";  
 })
+
+let mobileMenuBtn = document.querySelector(".Mobile-Menu-Btn");
+let mobileMenu = document.querySelector(".Mobile-Menu");
+mobileMenuBtn.addEventListener("click", function(){
+
+mobileMenu.classList.toggle("Mobile-Menu-drop");
+
+
+});
+
